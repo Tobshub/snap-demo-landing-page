07b29004bf24d1754aa7060f418f31bd718ae29e
Tobani <tobanigeorge@gmail.com>
2022-08-18 00:05:52 +0100

    bug fixes

--- script.js
+++ script.js
@@ -17,34 +17,36 @@ let hover_menu = document.querySelectorAll('.dropdown');
 let hover_menu_items = document.querySelectorAll('.dropdown-items');
 
 
-    hover_menu[0].addEventListener('click',()=>{
-      if(!hover_menu[0].classList.contains('dropup')){
+    hover_menu[0].addEventListener('mouseover',()=>{
+        hover_menu[0].style.height = '150px';
         hover_menu_items[0].style.height = 'max-content';
         hover_menu_items[0].style.padding = '10px';
         hover_menu[0].classList.remove('dropdown');
         hover_menu[0].classList.add('dropup');
-       }
-       else if(hover_menu[0].classList.contains('dropup')){
+
+    })
+    hover_menu[0].addEventListener('mouseout',()=>{
+      hover_menu[0].style.height = 'max-content';
         hover_menu_items[0].style.height = '0';
         hover_menu_items[0].style.padding = '0';
         hover_menu[0].classList.remove('dropup');
         hover_menu[0].classList.add('dropdown');
-       }
     })
 
-    hover_menu[1].addEventListener('click',()=>{
-      if(!hover_menu[1].classList.contains('dropup')){
+    hover_menu[1].addEventListener('mouseover',()=>{
+      hover_menu[1].style.height = '115px';
         hover_menu_items[1].style.height = 'max-content';
         hover_menu_items[1].style.padding = '10px';
         hover_menu[1].classList.remove('dropdown');
         hover_menu[1].classList.add('dropup');
-       }
-       else if(hover_menu[1].classList.contains('dropup')){
-        hover_menu_items[1].style.height = '0';
+       
+    })
+    hover_menu[1].addEventListener('mouseout',()=>{
+      hover_menu[1].style.height = 'max-content';
+      hover_menu_items[1].style.height = '0';
         hover_menu_items[1].style.padding = '0';
         hover_menu[1].classList.remove('dropup');
         hover_menu[1].classList.add('dropdown');
-       }
     })
 
 
